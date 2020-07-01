a48afa568b7eee720e988af3f4a3e9e9cbb76a8a
Jackson Oppenheim <jacksonop@gmail.com>
2020-06-30 21:55:36 -0500

    set up db

---
+++ connection.js
@@ -0,0 +1,9 @@
+const connection = mysql.createConnection({
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
+  port: process.env.PORT,
+});
+
+module.exports = connection;
